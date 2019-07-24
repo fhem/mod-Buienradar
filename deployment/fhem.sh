065f0b4fa9365abc8291c9c486fa54270a4076a0
Christoph Morrison <post@christoph-jeschke.de>
2019-07-24 22:12:08 +0200

    DateTime is now not longer necessary, so I deleted it from the vagrant deployment

--- deployment/fhem.sh
+++ deployment/fhem.sh
@@ -46,7 +46,7 @@ function add_sudoers {
 }
 
 function perl_modules {
-    cpanm install DateTime App::cpanoutdated Perl::PrereqScanner::NotQuiteLite IO::Socket::INET6 Socket6 Cpanel::JSON::XS
+    cpanm install App::cpanoutdated Perl::PrereqScanner::NotQuiteLite IO::Socket::INET6 Socket6 Cpanel::JSON::XS
 }
 
 perl_modules
