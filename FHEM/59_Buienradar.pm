96a91eb8bbba655e3578c4f6e1268ae5f460a93c
Christoph Morrison <post@christoph-jeschke.de>
2020-04-13 15:22:54 +0200

    [FHEMBUIENRADAR-50] fixed issues with missing =over sections and moved the top comment section to

--- FHEM/59_Buienradar.pm
+++ FHEM/59_Buienradar.pm
@@ -1,35 +1,37 @@
-# This is free and unencumbered software released into the public domain.
+=pod
 
-#
-#  59_Buienradar.pm
-#       2018 lubeda
-#       2019 ff. Christoph Morrison, <fhem@christoph-jeschke.de>
+ This is free and unencumbered software released into the public domain.
 
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
+  59_Buienradar.pm
+       2018 lubeda
+       2019 ff. Christoph Morrison, <fhem@christoph-jeschke.de>
 
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
+ Anyone is free to copy, modify, publish, use, compile, sell, or
+ distribute this software, either in source code form or as a compiled
+ binary, for any purpose, commercial or non-commercial, and by any
+ means.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+ In jurisdictions that recognize copyright laws, the author or authors
+ of this software dedicate any and all copyright interest in the
+ software to the public domain. We make this dedication for the benefit
+ of the public at large and to the detriment of our heirs and
+ successors. We intend this dedication to be an overt act of
+ relinquishment in perpetuity of all present and future rights to this
+ software under copyright law.
 
-#  For more information, please refer to <http://unlicense.org/>
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
 
-# See also https://www.buienradar.nl/overbuienradar/gratis-weerdata
+  For more information, please refer to <http://unlicense.org/>
 
+ See also https://www.buienradar.nl/overbuienradar/gratis-weerdata
+
+=cut
 
 package FHEM::Buienradar;
 
@@ -516,7 +518,11 @@ END_MESSAGE
     return ($as_html);
 }
 
+=over 1
+
 =item C<FHEM::Buienradar::GChart>
+
+=back
 
 C<FHEM::Buienradar::GChart> returns the precipitation data from buienradar.nl as PNG, renderd by Google Charts as
 a PNG data.
@@ -600,11 +606,14 @@ sub GChart {
 
 CHART
 }
+=over 1
 
 =item C<FHEM::Buienradar::LogProxy>
 
 C<FHEM::Buienradar::LogProxy> returns FHEM log look-alike data from the current data for using it with
 FTUI. It returns a list containing three elements:
+
+=back
 
 =over 1
 
