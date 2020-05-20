4d77265742dde6b52fbef22c3c6f4d7626401e18
Christoph 'knurd' Morrison <git@christoph-jeschke.de>
2020-05-20 09:11:27 +0200

    #254 #253 #252 #251 Fixed parentheses mashup

--- FHEM/59_Buienradar.pm
+++ FHEM/59_Buienradar.pm
@@ -678,10 +678,10 @@ sub ParseHttpResponse {
             $hash->{'.SERIALIZED'} = Storable::freeze(\%precipitation_forecast);
 
             ::readingsBeginUpdate($hash);
-            ::readingsBulkUpdate( $hash, 'state', (($rainNow) ? sprintf '%.3f', $rainNow : 'unknown');
+            ::readingsBulkUpdate( $hash, 'state', $rainNow ? sprintf '%.3f', $rainNow : 'unknown');
             ::readingsBulkUpdate( $hash, 'rainTotal', sprintf '%.3f', $rainTotal);
             ::readingsBulkUpdate( $hash, 'rainAmount', sprintf '%.3f', $rainAmount);
-            ::readingsBulkUpdate( $hash, 'rainNow', (($rainNow) ? sprintf( '%.3f', $rainNow) : 'unknown'));
+            ::readingsBulkUpdate( $hash, 'rainNow', $rainNow ? sprintf '%.3f', $rainNow : 'unknown');
             ::readingsBulkUpdate( $hash, 'rainLaMetric', $rainLaMetric );
             ::readingsBulkUpdate( $hash, 'rainDataStart', POSIX::strftime '%R', localtime $dataStart);
             ::readingsBulkUpdate( $hash, 'rainDataEnd', POSIX::strftime '%R', localtime $dataEnd );
